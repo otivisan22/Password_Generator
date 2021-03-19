e8d69d14caed78d9156546165ff13f27b10f8e6a
Otilia Visan <otilia.visan22@gmail.com>
2021-03-19 21:53:59 +0000

    declared the characters for the password

--- script.js
+++ script.js
@@ -1,13 +1,74 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+const generatePassword = function () {};
+
+// Special characters for the generated password
+const lowerCase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+const upperCase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const SpecialCharacters = ["?", "!", ",", "@", "&", "%"];
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
