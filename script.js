6577b2c9a002ba7d018827f69b45af7134b943d5
Otilia Visan <otilia.visan22@gmail.com>
2021-03-21 11:32:17 +0000

    fixed the mistake

--- script.js
+++ script.js
@@ -60,6 +60,7 @@ const upperCase = [
 ];
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const SpecialCharacters = ["?", "!", ",", "@", "&", "%", "*", ";"];
+
 // Declare function that generates the password, Declared prompt function
 const generatePassword = function () {
   let islowerCase;
